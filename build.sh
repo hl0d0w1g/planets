b9fdc59a96d37f5fe339852123f35e86e9aa2d72
lcambero <camberopiqueras@gmail.com>
2018-10-07 19:26:06 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+# Compile Elm files
 elm-make ./src/Planets/Planets.elm --output=./src/planets.js --warn
+# Locate the repository
+LOC='file://'$PWD'/indiex.html'
+# Open in browser
+# Check if Google Chrome exists
+if ! type 'google-chrome' > /dev/null; then
+  	echo 'Google Chrome is not installed, the app can be opened with other browsers by visiting the following url: '$LOC
+else
+	google-chrome $LOC
+fi
